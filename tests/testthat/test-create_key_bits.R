25da9f48cb91346aee9177cc59decccbe175feb3
James Campbell <dr.j.campbell@gmail.com>
2023-07-20 23:00:59 +0100

    ready to go

---
+++ tests/testthat/test-create_key_bits.R
@@ -0,0 +1,6 @@
+test_that("create_key_bits works", {
+  expected_key_bits = c(1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0)
+  img = matrix(rep(0,100), nrow = 10)
+  key_bits = as.numeric(create_key_bits(key = "test", image = img))
+  expect_equal(key_bits, expected_key_bits)
+})
