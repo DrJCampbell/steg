24b9738271b2deafbc0ef52ce56c8564548919c9
James Campbell <dr.j.campbell@gmail.com>
2023-07-30 23:46:16 +0100

    sorting out channels

--- R/write_encrypted_image.R
+++ R/write_encrypted_image.R
@@ -16,8 +16,7 @@ write_encrypted_image = function(file, message, image, key_bits){
   # bits and xor them. Store the result in the least
   # sig bit. To reverse this, xor the last two bits and
   # store the result * 255 in a decrypted matrix
-  if(FALSE){
-    #if(!all(dim(message) == dim(image))){
+  if(!all(dim(message)[1] == dim(image)[1] & dim(message)[2] == dim(image)[2])){
     stop(
       paste0(
         "Image matricies need to be the same size. image size is ",
@@ -32,18 +31,41 @@ write_encrypted_image = function(file, message, image, key_bits){
       )
     )
   }
+  # modify the
+  if(!is.na(dim(image)[3])){
+    channel_select = matrix(
+      rep(
+        c(1,2,3),
+        times=(length(as.numeric(image)) / dim(image)[3])
+      )[1:(dim(image)[1] * dim(image)[2])],
+      nrow=nrow(image)
+    )
+  }
   for(r in 1:nrow(image)){
     for(c in 1:ncol(image)){
       # convert values to bits
       message_bits = intToBits(message[r,c])
-      image_bits = intToBits(image[r,c])
+      # check if single (bw) channel or three (rgb)
+      if(is.na(dim(image)[3])){
+        # image is single-channel grey scale
+        image_bits = intToBits(image[r,c])
+      } else {
+        image_bits = intToBits(image[r,c, channel_select[r,c]])
+      }
+
+
       # xor the last bit of the message with
-      # the second to last bit of the image
+      # the last bit of the image
       image_bits[1] = xor(message_bits[1],key_bits[r,c])
       # convert the image_bits back to range 0:1
       # store the result in the last bit of
       # the image
-      image[r,c] = packBits(image_bits, type = "integer") / 255
+      modified_bit = packBits(image_bits, type = "integer") / 255
+      if(is.na(dim(image)[3])){
+        image[r,c] = modified_bit
+      } else {
+        image[r,c, channel_select[r,c]] = modified_bit
+      }
     }
   }
   png::writePNG(
